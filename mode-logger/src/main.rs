bf78ece7c9226c70db723a1e82673a0d2e7325ea
Erik Lilja <erik.lilja@funnel.io>
2022-11-07 16:35:11 +0100

    work

--- mode-logger/src/main.rs
+++ mode-logger/src/main.rs
@@ -1,6 +1,8 @@
 use colored::Colorize;
 use regex::Regex;
 use zellij_tile::prelude::*;
+use unicode_segmentation::UnicodeSegmentation;
+
 
 #[derive(Default)]
 struct State {
@@ -45,30 +47,64 @@ impl ZellijPlugin for State {
     }
 
     fn render(&mut self, rows: usize, cols: usize) {
-        let sep = "\u{E0B0}";
+        let left_sep = "\u{E0B0}";
+        let right_sep = "\u{E0B2}";
         let mut s = String::new();
 
         self.tabs.iter().enumerate().for_each(|(idx, tab)| {
             if tab.active {
                 s.push_str(&format!(
                     "{}{}{}",
-                    sep.on_yellow().black(),
+                    left_sep.on_yellow().black(),
                     format!(" {} ", tab.name).on_yellow().black(),
-                    sep.yellow()
+                    left_sep.yellow()
                 ));
-                s.push_str(&sep.red().on_black());
-                s.push_str(&("asdf".blue()));
+                // s.push_str(&format!("{}", leftSep.red().on_black()));
             } else {
                 s.push_str(&format!(
                     "{}{}",
-                    sep.on_bright_black().black(),
-                    format!(" {} ", tab.name).on_bright_black().on_bright_black(),
+                    left_sep.on_bright_black().black(),
+                    format!(" {} ", tab.name).on_bright_black(),
                 ));
-                s.push_str(&sep.on_bright_black().bright_black());
+                s.push_str(&format!("{}", left_sep.bright_black()));
             }
             // s.push_str(&" ");
             // if self.tabs.len() > idx+1
         });
-        println!("Hi mom! rows:{} cols:{}{}{}", rows, cols, s, "blu2".blue());
+        let mode = self.mode_info.mode;
+        /*
+         * <-----------------------------> col     - e.g. 160
+         * <--------------|--------------> middle = col / 2
+         * <text>                          s.len() - e.g. 60
+         * <text><-----------------------> padding needed = col - s.len()
+         *       <------------>            padding needed left side = padding needed/2
+         *
+         * <-----------><text><---------->
+         */
+        let text_size: usize = self.tabs.iter().map(|i| i.name.len() + 5).sum();
+        let col_diff = cols - text_size;
+        let zellij_human_readable_mode = format!("{:?}", mode).to_uppercase();
+        let zellij_human_readable_mode_padded = format!("{} ", zellij_human_readable_mode).black().on_green();
+        let left = "";
+        let right = format!(
+            "{right_sep_uncolored}{right_sep_colored}{zellijHumanReadableModePadded}",
+            right_sep_uncolored=right_sep.green().on_black(),
+            right_sep_colored=right_sep.green().on_green(),
+            zellijHumanReadableModePadded=zellij_human_readable_mode_padded,
+
+        );
+
+        let left_padding = " ".repeat((col_diff-left.len()) / 2).on_black();
+        let right_padding = " ".repeat((col_diff-(zellij_human_readable_mode.len()+3)) / 2).on_black();
+        println!("{} {} {} {}", cols, col_diff, zellij_human_readable_mode.len(), (col_diff-(zellij_human_readable_mode.len()+3)));
+
+        println!(
+            "{left}{left_padding}{center}{right_padding}{right}",
+             left_padding=left_padding,
+             right_padding=right_padding,
+             center=s,
+             left=left,
+             right=right,
+         );
     }
 }
